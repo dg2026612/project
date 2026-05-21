cb73006a53f2d731311a08e6a7df381eaf29b115
dg2026612 <dg2026612@danggok.hs.kr>
2026-05-21 09:05:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-box(pos=vec(0, 0, 0), color=color.vec(247,218,234))
+box(pos=vec(0, 0, 0),length=0.2, height=0.4, width=1.1, color=color.blue)
+box(pos=vec(0, 0, 0), color=vec(247/255,218/255,234/255))

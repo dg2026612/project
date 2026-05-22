a4baac04b618a583fcbc6c2ef3b860755545c7b5
dg2026612 <dg2026612@danggok.hs.kr>
2026-05-22 10:10:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-box(pos=vec(0, 0, 0),length=0.2, height=0.4, width=1.1, color=color.blue)
-box(pos=vec(0, 0, 0), color=vec(247/255,218/255,234/255))
+for i in range(100) : 
+    box(pos=vec(90,i,180),size=vec(80,80,70), color=vec(0/255,0/255,i/255))
+box(pos=vec(0, 0, 0),size=vec(400,400,400), color=vec(247/255,218/255,234/255))
+for i in range(100) : 
+    box(pos=vec(-90,i,180),size=vec(80,80,70), color=vec(0/255,0/255,i/255))
+box(pos=vec(90, 100, 20),size=vec(63,63,400), color=vec(1,1,1))
+box(pos=vec(-90, 100, 20),size=vec(63,63,400), color=vec(1,1,1))

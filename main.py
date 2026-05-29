93340cdf432193dc57c0fd342f087416ec540aeb
dg2026612 <dg2026612@danggok.hs.kr>
2026-05-29 10:07:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ for i in range(100) :
 box(pos=vec(0, 0, 0),size=vec(400,400,400), color=vec(247/255,218/255,234/255))
 box(pos=vec(90, 100, 20),size=vec(63,63,400), color=vec(1,1,1))
 box(pos=vec(-90, 100, 20),size=vec(63,63,400), color=vec(1,1,1))
-text(text='Kirby', align='center', color=vec(1,1,1) , pos=vec(0,400,0),  width=100)
 
-for i in range(250) :
-    ring(axis = vec(0,0,1), pos=vec(0, -110, i), radius = 50, color=vec(247/255,218/255,234/255), )
+for i in range(220) :
+    ring(axis = vec(0,0.2,1), pos=vec(0, -110, i), radius = 50, color=vec(247/255,218/255,234/255))
     
-ellipsoid(pos=vec(0, -110, 260), length=40, height=10, width=30)
+ellipsoid(pos=vec(0, -130, 200), length=80, height=70, width=70,color=vec(227/255,53/255,96/255)) 
+cylinder(pos=vec(0, -110, 130), axis=vec(0, 0, 1), color=vec(128/255,31/255,14/255),size=vec(80,100,100))
+box(pos=vec(150, 0, 0),size=vec(200,200,200), color=vec(247/255,218/255,234/255),  axis=vec(0,0,0.7))
